42377e1c7a5ec121ea9cdbaae30205c299f00d31
Sonwanseo <sonwanseo@gmail.com>
2021-02-15 11:01:30 +0900

    Chapter 8

---
+++ Chapter 8/연습 문제1.js
@@ -0,0 +1,21 @@
+function base10ToString(n) {
+    let binaryString = "";
+
+    function base10ToStringHelper(n) {
+        if (n < 2) {
+            binaryString += n;
+            return;
+        } else {
+            base10ToStringHelper(Math.floor(n / 2));
+            base10ToStringHelper(n % 2);
+        }
+    }
+    base10ToStringHelper(n);
+
+    return binaryString;
+}
+
+console.log(base10ToString(232)); // 11101000
+
+// 시간 복잡도: O(log2(n))
+// 공간 복잡도: O(log2(n))
